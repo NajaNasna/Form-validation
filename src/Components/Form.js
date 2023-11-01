cb3bf458e0f92ac1c377546595c4f968576ae4a2
NajaNasna <najanazir453@gmail.com>
2023-11-01 12:21:19 +0530

    form modified

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useFormik } from 'formik'
 import { signup } from './Yup'
 
 function Form() {
+
+    const nameInput = useRef(null)
+    const searchInput = useRef(null)
 
     const initialValues = {
         Name: "",
@@ -26,6 +29,12 @@ function Form() {
     console.log(errors);
     // console.log(store);
 
+
+    useEffect(()=>{
+        nameInput.current.focus()
+    },[])
+
+
     return (
 
         <div className="main">
@@ -35,28 +44,28 @@ function Form() {
             <div className="row">
 
                 <div className="left col-md-4" >
-                    <img src="./images/desola-lanre-ologun-zYgV-NGZtlA-unsplash.jpg" alt="" />
+                    <img src="./images/login-image.avif" alt="" />
                 </div>
 
                 <div className="right col-md-8" >
-                    <form action="" onSubmit={handleSubmit}>
+                    <form action="" >
                         <label class="form-label">Name</label>
-                        <input type="text" id="name" name='Name' value={values.Name} class="form-control" onChange={handleChange} onBlur={handleBlur} />
+                        <input type="text" id="name" name='Name' value={values.Name} ref={nameInput} autoComplete='off' class="form-control"  onChange={handleChange} onBlur={handleBlur} />
                         {errors.Name && touched.Name ? (<p className="error-text">{errors.Name}</p>) : null}
 
                         <label class="form-label">Email</label>
-                        <input type="email" id="email" name='Email' value={values.Email} class="form-control" onChange={handleChange} onBlur={handleBlur} />
+                        <input type="email" id="email" name='Email' value={values.Email}  autoComplete='off' class="form-control" onChange={handleChange} onBlur={handleBlur} />
                         {errors.Email && touched.Email ? (<p className="error-text">{errors.Email}</p>) : null}
 
                         <label class="form-label">Password</label>
-                        <input type="text" id="password" name='Password' value={values.Password} class="form-control" onChange={handleChange} onBlur={handleBlur} />
+                        <input type="text" id="password" name='Password' value={values.Password}  autoComplete='off' class="form-control" onChange={handleChange} onBlur={handleBlur} />
                         {errors.Password && touched.Password ? (<p className="error-text">{errors.Password}</p>) : null}
 
                         <label class="form-label">Re-Password</label>
-                        <input type="text" id="repassword" name='Repassword' value={values.Repassword} class="form-control" onChange={handleChange} onBlur={handleBlur} />
+                        <input type="text" id="repassword" name='Repassword' value={values.Repassword}   autoComplete='off' class="form-control" onChange={handleChange} onBlur={handleBlur} />
                         {errors.Repassword && touched.Repassword ? (<p className="error-text">{errors.Repassword}</p>) : null}
 
-                        <button className='btn btn-primary my-3'>Submit</button>
+                        <button className='btn btn-primary my-3' onSubmit={handleSubmit}>Submit</button>
                     </form>
                 </div>
             </div>
